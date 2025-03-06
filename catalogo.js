6e6daa5855bc9e13c46e29fa205ea1c7e100ebbe
YordanDevZD <YordanDevs@gmail.com>
2025-03-06 18:37:15 +0000

    Base de Datos

--- catalogo.js
+++ catalogo.js
@@ -1,22 +1,63 @@
 export const CATALOGO_ITEMS = [
+    //Series
+    { nombre: "Breaking Bad", tipo: "tv", categoria: "series" },
+    { nombre: "Los Hombres De Paco", tipo: "tv", categoria: "series" },
+    { nombre: "A Que Estas Esperando", tipo: "tv", categoria: "series" },
+    //Novelas Turkas
     { nombre: "Kara Sevda", tipo: "tv", categoria: "novelas" },
+
+    //Novelas Latinas
     { nombre: "Avenida Brasil", tipo: "tv", categoria: "novelas" },
     { nombre: "La Reina del Sur", tipo: "tv", categoria: "novelas" },
-    { nombre: "Breaking Bad", tipo: "tv", categoria: "series" },
     { nombre: "Belleza Fatal", tipo: "tv", categoria: "novelas" },
-    { nombre: "Los Hombres De Paco", tipo: "tv", categoria: "series" },
-    { nombre: "Interestelar", tipo: "movie", categoria: "peliculas" },
-    { nombre: "Culpa Mia", tipo: "movie", categoria: "peliculas" },
     { nombre: "La Jefa", tipo: "tv", categoria: "novelas" },
     { nombre: "Las Hijas De La Señora Garcia", tipo: "tv", categoria: "novelas" },
     { nombre: "Los CasaBlanca", tipo: "tv", categoria: "novelas" },
     { nombre: "Sed De Venganza", tipo: "tv", categoria: "novelas" },
     { nombre: "Valle Salvaje", tipo: "tv", categoria: "novelas" },
-    { nombre: "A Que Estas Esperando", tipo: "tv", categoria: "series" },
     { nombre: "Amar, Donde El Amor Teje Sus Redes", tipo: "tv", categoria: "novelas" },
     { nombre: "Amor Amargo", tipo: "tv", categoria: "novelas" },
-    { nombre: "Caminos Cruzados ", tipo: "tv", categoria: "novelas" },
+    { nombre: "Caminos Cruzados", tipo: "tv", categoria: "novelas" },
     { nombre: "Doctora Lucia", tipo: "tv", categoria: "novelas" },
     { nombre: "El Angel de Aurora", tipo: "tv", categoria: "novelas" },
-    { nombre: "Valle Salvaje", tipo: "tv", categoria: "novelas" },
+    { nombre: "El Conde Amor y Honor", tipo: "tv", categoria: "novelas" },
+    { nombre: "El Precio de Amarte", tipo: "tv", categoria: "novelas" },
+    { nombre: "Fugitivas", tipo: "tv", categoria: "novelas" },
+    { nombre: "Klass 95", tipo: "tv", categoria: "novelas" },
+    { nombre: "La Historia de Juana", tipo: "tv", categoria: "novelas" },
+    { nombre: "La Vida Entera", tipo: "tv", categoria: "novelas" },
+    { nombre: "Ligeramente Diva", tipo: "tv", categoria: "novelas" },
+    { nombre: "Papa Por Conveniencia", tipo: "tv", categoria: "novelas" },
+    { nombre: "TIERRA DE DESEOS", tipo: "tv", categoria: "novelas" },
+
+    { nombre: "El Maleficio", tipo: "tv", categoria: "novelas" },
+    { nombre: "Golpe de Suerte", tipo: "tv", categoria: "novelas" },
+    { nombre: "Marea de Pasiones", tipo: "tv", categoria: "novelas" },
+    { nombre: "Nadie Como Tu", tipo: "tv", categoria: "novelas" },
+    { nombre: "PACTO DE  SILENCIO", tipo: "tv", categoria: "novelas" },
+    { nombre: "Solo Confia", tipo: "tv", categoria: "novelas" },
+    { nombre: "Vuelve A Mi", tipo: "tv", categoria: "novelas" },
+    { nombre: "ACORRALADA", tipo: "tv", categoria: "novelas" },
+    { nombre: "Amar y Vivir", tipo: "tv", categoria: "novelas" },
+    { nombre: "AMARTE ASI FRIJOLITO", tipo: "tv", categoria: "novelas" },
+    { nombre: "Amor Perfecto", tipo: "tv", categoria: "novelas" },
+    { nombre: "Ana de Nadie", tipo: "tv", categoria: "novelas" },
+    { nombre: "Anita no te rajes ", tipo: "tv", categoria: "novelas" },
+    { nombre: "Buenos dias Veronica", tipo: "tv", categoria: "novelas" },
+    { nombre: "CABO", tipo: "tv", categoria: "novelas" },
+    { nombre: "Cafe Con Aroma de Mujer ", tipo: "tv", categoria: "novelas" },
+    { nombre: "Corran Por Sus Vidas", tipo: "tv", categoria: "novelas" },
+    { nombre: "COSITA LINDA", tipo: "tv", categoria: "novelas" },
+    { nombre: "CUANDO ME ENAMORO", tipo: "tv", categoria: "novelas" },
+    { nombre: "CUIDADO CON EL ANGEL", tipo: "tv", categoria: "novelas" },
+    { nombre: "DESPERTAR CONTIGO", tipo: "tv", categoria: "novelas" },
+    { nombre: "Devuelveme La Vida", tipo: "tv", categoria: "novelas" },
+
+    //Peliculas
+    { nombre: "Interestelar", tipo: "movie", categoria: "peliculas" },
+    { nombre: "Culpa Mia", tipo: "movie", categoria: "peliculas" },
+
+    //Shows
+    
+     
 ];
