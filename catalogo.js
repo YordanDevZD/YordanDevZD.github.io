e15a6e7523cb40b6446ead0c05ecc356a95accb4
YordanDevZD <YordanDevs@gmail.com>
2025-03-05 21:45:10 -0500

    Update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,9 +1,7 @@
 export const CATALOGO_ITEMS = [
     { nombre: "Kara Sevda", tipo: "tv", categoria: "novelas" },
     { nombre: "Avenida Brasil", tipo: "tv", categoria: "novelas" },
-    { nombre: "Muhteşem Yüzyıl", tipo: "tv", categoria: "novelas" },
     { nombre: "La Reina del Sur", tipo: "tv", categoria: "novelas" },
     { nombre: "Breaking Bad", tipo: "tv", categoria: "series" },
-    { nombre: "Inception", tipo: "movie", categoria: "peliculas" },
     { nombre: "Belleza Fatal", tipo: "tv", categoria: "novelas" }
 ];
