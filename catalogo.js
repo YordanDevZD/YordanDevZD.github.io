a7985c96d1b83dd2ca4258aa457ae9cfb6478ea9
YordanDevZD <YordanDevs@gmail.com>
2025-03-05 21:46:26 -0500

    Update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -3,5 +3,6 @@ export const CATALOGO_ITEMS = [
     { nombre: "Avenida Brasil", tipo: "tv", categoria: "novelas" },
     { nombre: "La Reina del Sur", tipo: "tv", categoria: "novelas" },
     { nombre: "Breaking Bad", tipo: "tv", categoria: "series" },
-    { nombre: "Belleza Fatal", tipo: "tv", categoria: "novelas" }
+    { nombre: "Belleza Fatal", tipo: "tv", categoria: "novelas" },
+    { nombre: "Los Hombres De Paco", tipo: "tv", categoria: "series" }
 ];
